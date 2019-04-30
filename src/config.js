50f48fe734331316acb4751df5596946347d4a20
yago batista silva <yagobatistasilva@gmail.com>
2019-04-29 23:20:47 -0700

    config production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     clinica: {
-        host: '127.0.0.1',
+        host: '192.168.1.100',
         port: 3050,
-        database: 'C:\\DB\\RJ_COPACABANA_CLINICA.FDB',
+        database: 'C:\\Unisolf\\RJ_COPACABANA_CLINICA.FDB',
         user: 'SYSDBA',
         password: 'masterkey',
         lowercase_keys: false,
@@ -10,9 +10,9 @@ module.exports = {
         pageSize: 4096,
     },
     contratos: {
-        host: '127.0.0.1',
+        host: '192.168.1.100',
         port: 3050,
-        database: 'C:\\DB\\RJ_COPACABANA_CONTRATOS.FDB',
+        database: 'C:\\Unisolf\\RJ_COPACABANA_CONTRATOS.FDB',
         user: 'SYSDBA',
         password: 'masterkey',
         lowercase_keys: false,
